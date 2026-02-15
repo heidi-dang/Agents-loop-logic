7e7e1bff856ce5db621ea584e98cb8a51bbc5a51
Heidi <heidi@dev.local>
2026-02-15 13:45:07 +1100

    Open WebUI polish: add ThinkingBubble, ToolCard, jump-to-bottom, smooth streaming, error states

---
+++ openwebui/src/lib/components/chat/Messages/Markdown/MarkdownInlineTokens/TextToken.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	export let token;
+	export let done = true;
+
+	let texts = [];
+	$: texts = (token?.raw ?? '').split(' ');
+</script>
+
+{#if done}
+	<span class="transition-opacity duration-200">{token?.raw}</span>
+{:else}
+	{#each texts as text, i}
+		<span 
+			class="inline" 
+			in:fade={{ duration: 80, delay: i * 10 }}
+		>
+			{text}{' '}
+		</span>
+	{/each}
+{/if}
